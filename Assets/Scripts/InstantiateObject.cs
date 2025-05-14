e1eab99fc88c501c2af1aa66118851ee86f8c3f6
KarenVite <156141759+KarenVite@users.noreply.github.com>
2025-05-13 20:42:50 -0600

    Implementacion boton con baile

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _objectToInstantiate;
+
+    public void Instantiate(Transform target)
+    {
+        Instantiate(_objectToInstantiate, target.position, Quaternion.identity);
+    }
+}
