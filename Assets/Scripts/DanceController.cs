5d95cfd19a42a43b0cf28492606e085b5ac4a02c
KarenVite <156141759+KarenVite@users.noreply.github.com>
2025-05-26 20:43:59 -0600

    TimerSound

--- Assets/Scripts/DanceController.cs
+++ Assets/Scripts/DanceController.cs
@@ -8,13 +8,24 @@ public class DanceController : MonoBehaviour
    [SerializeField]
    private UnityEvent _onSelectDance;
 
+   [SerializeField]
+   private Animator _characterAnimator;
+   private SoundData _currentSoundData;
+
    public void ActivateSelectedDance()
    {
-    _onActivateSelectedDance.Invoke();
+      _onActivateSelectedDance.Invoke();
    }
 
-   public void SelectDance()
+   public void SelectDance(SoundData soundData)
    {
-    _onSelectDance.Invoke();
+      _currentSoundData = soundData;
+      _onSelectDance.Invoke();
+   }
+
+   public void StartDance()
+   {
+      _characterAnimator.Play(_currentSoundData.danceName);
+      SoundManager.instance.PlayMusic(_currentSoundData.musicName);
    }
 }
---
+++ Assets/Scripts/NotesChecker.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class NotesChecker : MonoBehaviour
+{
+    private List<GameObject> _notes = new List<GameObject>();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Note"))
+        {
+            _notes.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Note"))
+        {
+            _notes.Remove(collision.gameObject);
+        }
+    }
+
+    public void DestroyNotes()
+    {
+        while (_notes.Count > 0)
+        {
+            GameObject note = _notes[0];
+            _notes.RemoveAt(0);
+            Destroy(note);
+        }
+        _notes.Clear();
+    }
+}
